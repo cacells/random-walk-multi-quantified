ad0c2dc10047795829ec19a076843a67fc65221a
beth <beth@qut.edu.au>
2010-06-15 11:26:13 +1000

    forgot to add

---
+++ testy.java
@@ -0,0 +1,19 @@
+
+public class testy {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+    Results[] saved = new Results[2];
+    saved[0] = new Results(2,3,4,5);
+    System.out.println("one "+saved[0].lineage);
+    Results saved2 = new Results(1,2,3,4);
+    System.out.println("two "+saved2.lineage);
+
+	
+	
+	}
+
+}
